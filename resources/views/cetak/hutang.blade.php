02338f2104b602cf56e4705dcc63e9c197a20603
Aliip-Akbar <aliip714@gmail.com>
2022-12-22 16:49:58 +0700

    Update

--- resources/views/cetak/hutang.blade.php
+++ resources/views/cetak/hutang.blade.php
@@ -5,7 +5,7 @@
     <div class="card shadow mt-4">
         <div class="card-header">
             <h1 class="h5 text-gray-800">Data Hutang</h1>
-            <h6 class="m-0 font-weight-bold text-primary">Data Master / Data Hutang</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Laporan / Data Hutang</h6>
         </div>
         <div class="card-body">
             <table class="table table-borderless data-table table-sm">
@@ -14,10 +14,10 @@
                         <th width="10px">#</th>
                         <th>Nama</th>
                         <th>Jumlah Hutang</th>
-                        <th>Total Bayar</th>
+                        <th>Sudah Dibayar</th>
                         <th>Sisa Hutang</th>
                         <th>Status</th>
-                        <th>Action</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody  class="table-striped"></tbody>
@@ -49,9 +49,10 @@
                                         </div>
                                         <div class="col">
                                             <div class="form-group">
-                                                <label for="name" class="col-sm control-label">Total Bayar</label>
+                                                <label for="name" class="col-sm control-label">Sudah Dibayar</label>
                                                 <div class="col-sm-12">
-                                                    <input type="text" class="form-control" id="total_bayar" name="total_bayar" placeholder="Masukkan Harga Hutang" value="" maxlength="50" required="">
+                                                    <input type="hidden" class="form-control" id="total_bayar" name="total_bayar" jAutoCalc="{total} + {bayar}" placeholder="Masukkan Harga Hutang" value="" maxlength="50" required="">
+                                                    <input type="text" class="form-control" id="total" name="total" placeholder="Masukkan Harga Hutang" value="" maxlength="50" required="">
                                                 </div>
                                             </div>
                                         </div>
@@ -59,21 +60,40 @@
                                             <div class="form-group">
                                                 <label for="name" class="col-sm control-label">Sisa Hutang</label>
                                                 <div class="col-sm-12">
-                                                    <input type="text" class="form-control" jAutoCalc="{jumlah_hutang} - {total_bayar}" id="sisa_hutang" name="sisa_hutang" placeholder="Masukkan Harga Hutang" value="" maxlength="50" required="">
+                                                    <input type="text" class="form-control" name="sisa" id="sisa"  placeholder="Masukkan Harga Hutang" value="" maxlength="50" required="">
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="name" class="col-sm control-label">Status</label>
+                                        <div class="row">
+                                            <div class="col">
+                                                 <div class="form-group">
+                                                <label for="name" class="col-sm control-label">Bayar</label>
                                                 <div class="col-sm-12">
-                                                    <select name="status" id="status" class="form-select">
-                                                        <option value="Lunas">Lunas</option>
-                                                        <option value="Belum Lunas">Belum Lunas</option>
-                                                    </select>
+                                                    <input type="text" class="form-control" id="bayar" value="0" name="bayar" placeholder="Masukkan Harga Hutang" value="" maxlength="50" required="">
+                                                </div>
+                                            </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="form-group">
+                                                    <label for="name" class="col-sm control-label">Hutang Sekarang</label>
+                                                    <div class="col-sm-12">
+                                                        <input type="text" class="form-control" jAutoCalc="{sisa} - {bayar}" id="sisa_hutang" name="sisa_hutang" placeholder="Masukkan Harga Hutang" value="" maxlength="50" required="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="form-group">
+                                                    <label for="name" class="col-sm control-label">Status</label>
+                                                    <div class="col-sm-12">
+                                                        <select name="status" id="status" class="form-select">
+                                                            <option value="Lunas">Lunas</option>
+                                                            <option value="Belum Lunas">Belum Lunas</option>
+                                                        </select>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
+
                                     </div>
                             <div class="col-sm-offset-2 col-sm-10">
                                 <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Simpan</button>
@@ -158,8 +178,8 @@
         $('#kd_Hutang').val(data.kd_Hutang).prop('readonly', true);
         $('#nama').val(data.nama).prop('readonly', false);
         $('#jumlah_hutang').val(data.jumlah_hutang).attr("disabled", false);
-        $('#total_bayar').val(data.total_bayar).attr("disabled", false);
-        $('#sisa_hutang').val(data.sisa_hutang).prop('readonly', false);
+        $('#total').val(data.total_bayar).attr("disabled", false);
+        $('#sisa').val(data.sisa_hutang).prop('readonly', false);
         $('#status').val(data.status).prop('readonly', false);
 
         })
